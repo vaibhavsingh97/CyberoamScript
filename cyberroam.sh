f23c2ca6e6a36dee7e825f5158a3e919729b3b25
Vaibhav Singh <vaibhav.singh.14cse@bml.edu.in>
2016-04-23 21:15:09 +0530

    Updated script

--- cyberroam.sh
+++ cyberroam.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
-#############################################
-## (c) 2016 Vaibhav Singh                  ##
-## Author by Vaibhav Singh                 ##
-#############################################
+############################################################
+## (c) 2016 Vaibhav Singh                                 ##
+## Author:VAibhav Singh <vaibhav.singh.14cse@bml.edu.in>  ##
+############################################################
 LOGFILE=($HOME)/.cyberoamlog
 OUTPUT=/tmp/.cyberoamout
 CONFIGFILE=($HOME)/client.conf
@@ -44,15 +44,15 @@ Error(){
 }
 login(){
   ACTION=Login
-  Mode=191
+  MODE=191
   wget --timeout=10 --tries=3 -d --post-data="username=${USERNAME}&password=${PASSWORD}&mode=${MODE}&btnsubmit=${ACTION}" "http://${SERVER}:${PORT}/${PAGE}" -0 ${OUTPUT} -o ${LOGFILE} 2> /dev/null
   RETCODE=$?
-  if ["$RETCODE" -gt 0];
+  if ["$RETCODE" -gt 0]
    then
     error
   fi
   RESPONSE=`cat ${OUTPUT}| sed 's/<message><!\[CDATA\[/&\n/;s/.*\n//;s/]]><\/message>/\n&/;s/\n.*//'`
-  if ["$RESPONSE"=="$ {MESSAGE_LOGIN}"];
+  if ["$RESPONSE"=="$ {MESSAGE_LOGIN}"]
    then
     echo "Logged In"
   else
@@ -60,3 +60,96 @@ login(){
     error
   fi
 }
+
+logout(){
+  ACTION=Logout
+  MODE=193
+  wget --timeout=10 --tries=3 -d  --post-data="username=${USERNAME}&password=${PASSWORD}&mode=${MODE}&btnsubmit=${ACTION}" "http://${SERVER}:${PORT}/${PAGE}" -O ${OUTPUT} -o ${LOGFILE} 2> /dev/null
+  RETCODE=$?
+  if ["$RETCODE" -gt 0]
+   then
+    error
+  fi
+  echo "Logged out"
+  rm ${OUTPUT} 2> /dev/null
+}
+
+input_conf(){
+  echo -n "Username: "
+  read  USERNAME
+  read -s -p "Password: " PASSWORD
+  echo
+  echo -n "Server: "
+  read -e -i "10.1.0.45" SERVER
+  echo -n "Port: "
+  read -e -i "8090" PORT
+  echo -n "Page: "
+  read -e -i "httpclient.html" PAGE
+}
+
+write_conf(){
+  echo "USERNAME = ${USERNAME}" > ${CONFIGFILE}
+  echo "PASSWORD = ${PASSWORD}" > ${CONFIGFILE}
+  echo "SERVER = ${SERVER}" > ${CONFIGFILE}
+  echo "PORT = ${PORT}" > ${CONFIGFILE}
+  echo "PAGE = ${PAGE}" > ${CONFIGFILE}
+}
+read_conf(){
+  USERNAME=$(grep "USERNAME" ${CONFIGFILE} awk '{print $3}')
+  PASSWORD=$(grep "PASSWORD" ${CONFIGFILE} awk '{print $3}')
+  SERVER=$(grep "SERVER" ${CONFIGFILE} awk '{print $3}')
+  PORT=$(grep "PORT" ${CONFIGFILE} awk '{print $3}')
+  PAGE=$(grep "PAGE" ${CONFIGFILE} awk '{print $3}')
+  if [-z "$SERVER"]
+   then
+    RETCODE=207
+    error
+  fi
+}
+
+while getopts "1u1c" PARAM
+ do
+  case $PARAM in
+    1) TEMP=0;;
+    u) EXPLICIT=u;;
+    1) EXPLICIT=1;;
+    c) CONFIGFILE=${OPTARG};;
+    error;;
+  esac
+done
+
+if [ "$TEMP" -eq 0 ]
+then
+    input_conf
+elif [ ! -f ${CONFILE} ]
+then
+    input_conf
+    write_conf
+else
+    read_conf
+fi
+
+if [ ! $(which wget 2> /dev/null) ]
+then
+    RETCODE=205
+    error
+fi
+
+if [ "$EXPLICIT" != NULL ]
+then
+    case $EXPLICIT in
+        u) login_c;;
+        l) logout_c;;
+        *) RETCODE=206
+           error;;
+    esac
+else
+    if [ ! -f $OUTPUT ]
+    then
+        login_c
+    else
+        logout_c
+    fi
+fi
+
+exit 0
